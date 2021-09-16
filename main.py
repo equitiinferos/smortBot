d8a1048faa19e557c38f893ab3c8b5207d4a0c4e
xerx <85109217+equitiinferos@users.noreply.github.com>
2021-09-16 13:23:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,46 @@ phrases = ['You may have n̶̿͌ö̷́̚ ̸̈́̅ỏ̵̀á̵͊t̷̀̅s̴̆̽', 'HA
 async def on_ready():
     print('bitch we in')
 
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}   
+
+def endSong(guild, path):
+    os.remove(path)                                   
+
+#define command to play music
+@bot.command(pass_context=True)
+async def play(ctx, url):
+    if not ctx.message.author.voice:
+        await ctx.send('you are not connected to a voice channel')
+        return
+
+    else:
+        channel = ctx.message.author.voice.channel
+
+    voice_client = await channel.connect()
+
+    guild = ctx.message.guild
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        file = ydl.extract_info(url, download=True)
+        path = str(file['title']) + "-" + str(file['id'] + ".mp3")
+
+    voice_client.play(discord.FFmpegPCMAudio(path), after=lambda x: endSong(guild, path))
+    voice_client.source = discord.PCMVolumeTransformer(voice_client.source, 1)
+
+    await ctx.send(f'**Your shitty song sir: **{url}')
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+    else:
+        await voice_client.disconnect()
+        print("Disconnected")    
+    
 #define help command
 @bot.command(name="help")
 async def help(ctx):
